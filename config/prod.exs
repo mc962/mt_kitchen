cc7e8fd26e8cf5a503990870b2b02b27896fc9d7
mc962 <11969409+mc962@users.noreply.github.com>
2022-08-31 23:25:01 -0400

    Set aws settings

--- config/prod.exs
+++ config/prod.exs
@@ -49,16 +49,4 @@ config :logger, level: :info
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
-config :waffle,
-  storage: Waffle.Storage.S3,
-  # or {:system, "AWS_S3_BUCKET"}
-  bucket: "lazy-kitchen",
-  # or {:system, "ASSET_HOST"}
-  asset_host: "https://alazykitchen.com"
-
-config :ex_aws,
-       access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
-       secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
-       json_codec: Jason
-
 # any configurations provided by https://github.com/ex-aws/ex_aws
