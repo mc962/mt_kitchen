6b092b81524341a046d2fcc9d22c8c109070ec76
mc962 <11969409+mc962@users.noreply.github.com>
2022-09-12 00:09:45 -0400

    Remove unused code

--- test/mt_kitchen_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MTKitchenWeb.PageControllerTest do
-  use MTKitchenWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to M and T's Kitchen</h1>"
-  end
-end
